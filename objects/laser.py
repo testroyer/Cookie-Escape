c6753a0c3bcde94e07cdf3bfdc8cd68fd38176a9
testroyer <alperozer2007@gmail.com>
2022-09-11 11:00:42 +0300

    fedup patch

--- objects/laser.py
+++ objects/laser.py
@@ -7,7 +7,7 @@ class Lazer(pygame.sprite.Sprite):
         self.image.fill("red")
         self.rect = self.image.get_rect(bottomright = pos)
         self.direction = pygame.math.Vector2(0,-1)
-        self.speed = 2
+        self.speed = 3
 
 
     def update(self):
